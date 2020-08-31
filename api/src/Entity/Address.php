cf084d0efb54ee71bd4ae695d6868420c060f615
Robert Zondervan <Robert@conduction.nl>
2020-08-31 17:32:40 +0200

    second draft

---
+++ api/src/Entity/Address.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AddressRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Ramsey\Uuid\UuidInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=AddressRepository::class)
+ */
+class Address
+{
+    /**
+     * @var UuidInterface The UUID identifier of this object
+     *
+     * @example e2984465-190a-4562-829e-a8cca81aa35d
+     *
+     * @Assert\Uuid
+     * @Groups({"read"})
+     * @ORM\Id
+     * @ORM\Column(type="uuid", unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
+     */
+    private $id;
+
+    /**
+     * @Groups({"read"})
+     * @ORM\Column(type="text")
+     */
+    private $type;
+
+    /**
+     * @Groups({"read"})
+     * @ORM\Column(type="text")
+     */
+    private $street;
+
+    /**
+     * @Groups({"read"})
+     * @ORM\Column(type="text")
+     */
+    private $houseNumber;
+
+    /**
+     * @Groups({"read"})
+     * @ORM\Column(type="string", length=255)
+     */
+    private $houseNumberAddition;
+
+    /**
+     * @Groups({"read"})
+     * @ORM\Column(type="string", length=255)
+     */
+    private $postalCode;
+
+    /**
+     * @Groups({"read"})
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @Groups({"read"})
+     * @ORM\Column(type="string", length=255)
+     */
+    private $country;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Company::class, mappedBy="addresses")
+     */
+    private $companies;
+
+    public function __construct()
+    {
+        $this->companies = new ArrayCollection();
+    }
+
+    public function getId(): ?UuidInterface
+    {
+        return $this->id;
+    }
+
+    public function setId(?UuidInterface $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(string $street): self
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getHouseNumber(): ?string
+    {
+        return $this->houseNumber;
+    }
+
+    public function setHouseNumber(string $houseNumber): self
+    {
+        $this->houseNumber = $houseNumber;
+
+        return $this;
+    }
+
+    public function getHouseNumberAddition(): ?string
+    {
+        return $this->houseNumberAddition;
+    }
+
+    public function setHouseNumberAddition(string $houseNumberAddition): self
+    {
+        $this->houseNumberAddition = $houseNumberAddition;
+
+        return $this;
+    }
+
+    public function getPostalCode(): ?string
+    {
+        return $this->postalCode;
+    }
+
+    public function setPostalCode(string $postalCode): self
+    {
+        $this->postalCode = $postalCode;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Company[]
+     */
+    public function getCompanies(): Collection
+    {
+        return $this->companies;
+    }
+
+    public function addCompany(Company $company): self
+    {
+        if (!$this->companies->contains($company)) {
+            $this->companies[] = $company;
+            $company->addAddress($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCompany(Company $company): self
+    {
+        if ($this->companies->contains($company)) {
+            $this->companies->removeElement($company);
+            $company->removeAddress($this);
+        }
+
+        return $this;
+    }
+}
--- api/src/Service/KvkService.php
+++ api/src/Service/KvkService.php
@@ -1,7 +1,146 @@
 <?php
 
+// Conduction/CommonGroundBundle/Service/KadasterService.php
+
+/*
+ * This file is part of the Conduction Common Ground Bundle
+ *
+ * (c) Conduction <info@conduction.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Service;
+
+use App\Entity\Adres;
+//use Doctrine\ORM\EntityManager;
+use App\Entity\Address;
+use App\Entity\Company;
+use App\Entity\TradeName;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use GuzzleHttp\Client;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidFactory;
+use Ramsey\Uuid\UuidInterface;
+use Symfony\Component\Cache\Adapter\AdapterInterface as CacheInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class KvkService
 {
+    private $config;
+    private $params;
+    private $client;
+    private $commonGroundService;
+    private $manager;
+    /**
+     * @var CacheInterface
+     */
+    private $cache;
 
+    public function __construct(ParameterBagInterface $params, CacheInterface $cache, EntityManagerInterface $manager)
+    {
+        $this->params = $params;
+        $this->cache = $cache;
+        $this->manager = $manager;
+
+        $this->client = new Client([
+            // Base URI is used with relative requests
+            'base_uri' => $this->params->get('common_ground.components')['kvk']['location'],
+            // You can set any number of default request options.
+            'timeout'  => 4000.0,
+            // This api key needs to go into params
+            'headers' => ['X-Api-Key' => $this->params->get('common_ground.components')['kvk']['apikey']],
+        ]);
+    }
+
+    public function getCompany(string $branchNumber){
+
+        $item = $this->cache->getItem('company_'.md5($branchNumber));
+        if($item->isHit()){
+            return $item->get();
+        }
+        $query = ['branchNumber'=>$branchNumber, 'branch'=>'false', 'mainBranch'=>'true'];
+        $response = $this->client->get('companies', ['query'=>$query])->getBody();
+//        var_dump($response);
+
+        $response = json_decode($response, true);
+        $response = $response['data']['items'][0];
+
+        $item->set($response);
+        $item->expiresAt(new DateTime('tomorrow 4:59'));
+        $this->cache->save($item);
+
+        return $item->get();
+    }
+    public function getObject($branch): Company
+    {
+//        var_dump($branch);
+        $company = new Company();
+        $company->setBranchNumber($branch['branchNumber']);
+        $company->setKvkNumber($branch['kvkNumber']);
+        $company->setRsin($branch['rsin']);
+
+        $company->setHasEntryInBusinessRegister($branch['hasEntryInBusinessRegister']);
+        $company->setHasNonMailingIndication($branch['hasNonMailingIndication']);
+
+        $company->setIsLegalPerson($branch['isLegalPerson']);
+        $company->setIsBranch($branch['isBranch']);
+        $company->setIsMainBranch($branch['isMainBranch']);
+
+        foreach($branch['addresses'] as $rawAddress){
+            $address = new Address();
+            $address->setType($rawAddress['type']);
+            $address->setStreet($rawAddress['street']);
+            $address->setHouseNumber($rawAddress['houseNumber']);
+            $address->setHouseNumberAddition($rawAddress['houseNumberAddition']);
+            $address->setPostalCode($rawAddress['postalCode']);
+            $address->setCity($rawAddress['city']);
+            $address->setCountry($rawAddress['country']);
+
+            $this->manager->persist($address);
+
+            $address->setId(Uuid::uuid4());
+            $this->manager->persist($address);
+
+            $company->addAddress($address);
+        }
+        if(key_exists('tradeNames', $branch)){
+            $company->setTradeNames($branch['tradeNames']);
+        }
+
+        // Let see what we got here in terms of object
+
+        $this->manager->persist($company);
+        $company->setId($branch['branchNumber']);
+        $this->manager->persist($company);
+
+        return $company;
+    }
+
+    public function getCompaniesOnSearchParameters($huisnummer, $postcode)
+    {
+        // Lets start with th getting of nummer aanduidingen
+        $now = new \Datetime();
+        $query = ['huisnummer'=>$huisnummer, 'postcode'=>$postcode, 'geldigOp'=>$now->format('Y-m-d')];
+        $nummeraanduidingen = $this->getNummeraanduidingen($query);
+
+        // Lets setup an responce
+        $responces = [];
+        // Then we need to enrich that
+        foreach ($nummeraanduidingen['nummeraanduidingen'] as $nummeraanduiding) {
+            $responces[] = $this->getObject($nummeraanduiding);
+        }
+
+        return $responces;
+    }
+
+    public function getCompanyOnBranchNumber($branchNumber) : Company
+    {
+
+        $company = $this->getCompany($branchNumber);
+
+        return $this->getObject($company);
+    }
 }
